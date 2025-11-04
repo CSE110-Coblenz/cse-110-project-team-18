e30dc41b7c051b90e49692804f0104626e4aaa29
Sydney <scsu@ucsd.edu>
2025-11-04 11:30:34 -0800

    added asteroid field game screen switching

--- src/screens/AsteriodFieldGameScreen/AsteroidFieldGameController.ts
+++ src/screens/AsteriodFieldGameScreen/AsteroidFieldGameController.ts
@@ -2,12 +2,53 @@ import { ScreenController } from '../../types.ts';
 import type { ScreenSwitcher } from '../../types.ts';
 import { AsteroidFieldGameView } from './AsteroidFieldGameView.ts';
 import { AsteroidFieldGameModel } from './AsteroidFieldGameModel.ts';
-import { STAGE_WIDTH } from '../../configs/GameConfig';
-import { PlayerManager } from '../../core/movement/PlayerManager';
-import { CollisionManager } from '../../core/collision/CollisionManager';
-import { spaceship_sprite } from '../../core/sprites/SpaceShipSprite';
-import { PlayerConfig } from '../../configs/PlayerConfig';
 
- export class AsteroidFieldGameController extends ScreenController {
+/**
+ * AsteroidFieldGameController - Handles asteroid field game interactions
+ */
+export class AsteroidFieldGameController extends ScreenController {
+	private view: AsteroidFieldGameView;
+	private model: AsteroidFieldGameModel;
 
- }
+	/**
+	 * Constructor for the AsteroidFieldGameController
+	 * @param screenSwitcher - The screen switcher
+	 */
+	constructor(screenSwitcher: ScreenSwitcher) {
+		super();
+		this.view = new AsteroidFieldGameView();
+		this.model = new AsteroidFieldGameModel();
+	}
+
+	/**
+	 * Get the view
+	 * @returns The view of the asteroid field game controller
+	 */
+	getView(): AsteroidFieldGameView {
+		return this.view;
+	}
+
+	/**
+	 * Show the asteroid field game screen controller
+	 */
+	override show(): void {
+		super.show();
+		// Additional initialization can be added here as needed
+	}
+
+	/**
+	 * Hide the asteroid field game screen controller
+	 */
+	override hide(): void {
+		super.hide();
+		// Cleanup can be added here as needed
+	}
+
+	/**
+	 * Update the asteroid field game screen controller
+	 * @param deltaTime - The time since the last frame in milliseconds
+	 */
+	override update(deltaTime: number): void {
+		// Game logic can be added here as needed
+	}
+}
---
+++ src/screens/AsteriodFieldGameScreen/AsteroidFieldGameModel.ts
@@ -0,0 +1,9 @@
+/**
+ * AsteroidFieldGameModel - The model for the asteroid field game screen
+ */
+export class AsteroidFieldGameModel {
+	constructor() {
+		// Model initialization can be added here as needed
+	}
+}
+
