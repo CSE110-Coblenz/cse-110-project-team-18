92df1d2ed94d4775e9c1e8f47d96b95e5332c607
Khoa Le <khoaduy1805@gmail.com>
2025-12-04 21:09:23 -0800

    Fix format

--- src/planets/venus/VenusGameController.ts
+++ src/planets/venus/VenusGameController.ts
@@ -39,9 +39,7 @@ export class VenusGameController extends ScreenController {
 		super();
 		this.screenSwitcher = screenSwitcher;
 		this.model = new VenusGameModel();
-		this.view = new VenusGameView(
-			() => this.handleSubmitAnswer()
-		);
+		this.view = new VenusGameView(() => this.handleSubmitAnswer());
 	}
 
 	getView(): VenusGameView {
